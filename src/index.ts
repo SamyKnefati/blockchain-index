131c029095e69cb55a3396c51fee218e2c4b3dcb
SamyKnefati <samy.knefati@edu.ece.fr>
2024-06-22 15:47:43 +0200

    update and changes on json storage

--- src/index.ts
+++ src/index.ts
@@ -7,41 +7,61 @@ import { ApiClient } from './generated/client';
 // src/index.ts
 const client = new ApiClient('18.203.155.106:57291', grpc.credentials.createInsecure());
 
-const request = {
-    number: 250708
-};
+// const request = {
+//     number: 250708
+// };
+const request = { account_id: { id: "0x96dbc8850ff58230" } };
 
-client.GetBlockByNumber(request, (error: grpc.ServiceError, response: any) => {
+client.GetBlockByNumber({ number: "259538" }, (error: grpc.ServiceError, response: any) => {
     if (error) {
         console.error(error);
         return;
+
     }
 
-    fs.writeFileSync('block.json', JSON.stringify(response, null, 2));
-    console.log('Response written to block.json');
-    // toString('hex');
-    // const hash = blockData.slice(0, 64);
-    // const hashBuffer = Buffer.from(hash, 'hex');
-    const blockData = Buffer.from(response.block);
-    const hash = blockData.slice(0, 64);
-    console.log(hash)
-    // const hashBuffer = Buffer.from(hash, 'hex');
+
+
+    // const buffer = JSON.stringify(response);
+    // const decodedString = Buffer.from(buffer, 'utf8');
+    // console.log(decodedString);
+
+    // // const buffer = response.toString();
+    // // const decodedString = buffer.toString('utf8');
+    // // console.log(decodedString);
+
+    // fs.writeFileSync('buffer.json', decodedString);
+
+    const jsonString = fs.readFileSync('buffer.json', 'utf8');
+
+    // Parse the JSON string into a JavaScript object
+    const jsonObject = JSON.parse(jsonString);
+
+    // Decode the data attribute
+    const buffer = Buffer.from(jsonObject.block.data);
+    const decodedString = buffer.toString('binary');
+    
+    console.log(buffer.toString('utf8'));
+    console.log(buffer.toString('ascii'));
+    console.log(buffer.toString('latin1'));
+    console.log(buffer.toString('hex'));
+    console.log(buffer.toString('base64'));
+    // fs.writeFileSync('data.json', buffer);
 })
 
-client.SyncState(request, request, (error: grpc.ServiceError, response: any) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    fs.writeFileSync('sync.json', JSON.stringify(response, null, 2));
-})
+// client.SyncState((error: grpc.ServiceError, response: any) => {
+//     if (error) {
+//         console.error(error);
+//         return;
+//     }
+//     fs.writeFileSync('sync.json', JSON.stringify(response, null, 2));
+// })
 
-client.GetAccountDetails(request, request, (error: grpc.ServiceError, response: any) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    fs.writeFileSync('acc.json', JSON.stringify(response, null, 2));
-})
+// client.GetAccountDetails(request, (error: grpc.ServiceError, response: any) => {
+//     if (error) {
+//         console.error(error);
+//         return;
+//     }
+//     fs.writeFileSync('acc.json', JSON.stringify(response, null, 2));
+// })
 
 
