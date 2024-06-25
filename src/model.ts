4472a69e9047af255123b9d5d33ae6725cda6a4c
SamyKnefati <samy.knefati@edu.ece.fr>
2024-06-25 11:59:53 +0200

    update on model for client class

--- src/model.ts
+++ src/model.ts
@@ -9,25 +9,13 @@ class GrpcClient {
         this.client = new ApiClient(serverAddress, grpc.credentials.createInsecure());
     }
 
-    getNotesById(noteId: string, callback: (error: grpc.ServiceError | null, response: any | null) => void) {
-        const note_id = { note_id: { id: noteId } };
-        this.client.GetNotesById(note_id, callback);
-    }
-
-    getBlockByNumber(number: string, callback: (error: grpc.ServiceError | null, response: any | null) => void) {
-        this.client.GetBlockByNumber({ number }, callback);
-    }
     getBlockHeaderByNumber(number: string, callback: (error: grpc.ServiceError | null, response: any | null) => void) {
         this.client.GetBlockHeaderByNumber({ number }, callback);
     }
-    syncState(callback: (error: grpc.ServiceError | null, response: any | null) => void) {
+    syncState(callback: (error: grpc.ServiceError | null, response: any | null) => number) {
         this.client.SyncState(callback);
     }
 
-    getAccountDetails(accountId: string, callback: (error: grpc.ServiceError | null, response: any | null) => void) {
-        const request = { account_id: { id: accountId } };
-        this.client.GetAccountDetails(request, callback);
-    }
 }
 
 export default GrpcClient;
